0031a916c457b4a06278345d9082092e6b81012e
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-09-05 16:42:47 -0300

    feat: add error message

--- src/components/FormSide/Form/Form.jsx
+++ src/components/FormSide/Form/Form.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import * as C from "./Form.styles";
 
 const Form = () => {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState(false);
+
+    const HandleSubmit = e => {
+        e.preventDefault();
+
+        if (!email || !email.includes("@")) {
+            setError(true);
+            return;
+        }
+
+        setEmail("");
+    };
+
+    const handleChangeEmail = e => {
+        setEmail(e.target.value)
+        setError(false)
+    }
+
     return (
-        <form>
-            <C.Label>Email Address</C.Label>
-            <C.Input type="email" placeholder="Digite seu email" />
+        <C.Form onSubmit={HandleSubmit}>
+            <C.FormControl>
+                <C.Label htmlFor="email">Email Address</C.Label>
+                {error ? <p>Valid email required</p> : ""}
+            </C.FormControl>
+            <C.Input
+                isError={error}
+                onChange={handleChangeEmail}
+                value={email}
+                type="text"
+                placeholder="Digite seu email"
+                name="email"
+            />
             <C.SubmitButton>Subscribe to monthly newsletter</C.SubmitButton>
-        </form>
+        </C.Form>
     );
 };
 
--- src/components/FormSide/Form/Form.styles.js
+++ src/components/FormSide/Form/Form.styles.js
@@ -1,5 +1,24 @@
 import { styled } from "styled-components";
 
+export const Form = styled.form`
+    width: 80%;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
+`;
+
+export const FormControl = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    p {
+        margin-top: 20px;
+        color: hsl(4, 100%, 67%);
+    }
+`;
+
 export const Label = styled.label`
     display: block;
     margin-top: 25px;
@@ -9,7 +28,7 @@ export const Label = styled.label`
 `;
 
 export const Input = styled.input`
-    width: 80%;
+    width: 100%;
     padding: 15px 10px;
     border-radius: 6px;
     border: none;
@@ -17,14 +36,11 @@ export const Input = styled.input`
     box-shadow: 0 0 0 1px #00000055;
     color: #36384e;
     font-weight: 600;
-
-    @media (max-width: 768px) {
-        width: 100%;
-    }
+    background-color: ${props => (props.isError ? "	#ff625755" : "#fff")};
 `;
 
 export const SubmitButton = styled.button`
-    width: 80%;
+    width: 100%;
     padding: 15px 10px;
     border-radius: 6px;
     border: none;
@@ -35,9 +51,5 @@ export const SubmitButton = styled.button`
 
     &:hover {
         background-image: linear-gradient(45deg, #fa3757 25%, #ff6257 75%);
-    }
-
-    @media (max-width: 768px) {
-        width: 100%;
     }
 `;
